72cb0fe341c02581126ae3414f89b151404c93f2
Peace-Ishimwe <peaceishimwem@gmail.com>
2025-01-13 20:16:25 +0200

    feat: product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -54,6 +54,23 @@ export class ProductService {
     if (!product) {
       throw new NotFoundException(`Product with ID ${id} not found`);
     }
+
+    const supplier = await this.prisma.supplier.findUnique({
+      where: { id: updateProductDto.supplierId }
+    })
+
+    if (!supplier) {
+      throw new NotFoundException("Supplier not found")
+    }
+
+    const stock = await this.prisma.stock.findUnique({
+      where: { id: updateProductDto.stockId }
+    })
+
+    if (!stock) {
+      throw new NotFoundException("Stock not found")
+    }
+
     return this.prisma.product.update({
       where: { id },
       data: updateProductDto,
